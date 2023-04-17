39fe9301211547cc73bd67d078b905cd3dccda0b
mglap <mglap@amazon.com>
2023-04-17 09:37:43 -0400

    fix module outputs

--- vpc-subnet/v1/infrastructure/src/outputs.tf
+++ vpc-subnet/v1/infrastructure/src/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
   description = "vpc id"
-  value       = aws_vpc.main.vpc_id
+  value       = aws_vpc.main.id
 }
 
 output "subnet_one_id" {
   description = "subnet one"
-  value       = aws_subnet.main.subnet_one_id
+  value       = aws_subnet.main.id
 }
 
 
