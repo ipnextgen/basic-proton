7ce7d3b343eea62725d10f1b4e68045de596dbe8
mglap <mglap@amazon.com>
2023-04-24 12:33:30 -0400

    AMI adjust

--- ec2/v1/instance_infrastructure/src/ec2.tf
+++ ec2/v1/instance_infrastructure/src/ec2.tf
@@ -1,7 +1,7 @@
 # Ceci deploi une instance EC2 du type specifié
 
 resource "aws_instance" "web" {
-  ami           = "ami-0e9d8c96b045dbc75"
+  ami           = "ami-07e17b58812e0c7c2"
   instance_type = var.instance
 
   tags = {
